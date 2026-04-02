2546b69eb5ca46661a0213fdb95a00efc8f15775
Phong Nguyen <nhphong1406@gmail.com>
2026-04-02 07:21:01 +0700

    Remove unused scripts

--- scripts/test-lua.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-cd "$(dirname "$0")/.."
-exec nvim --headless -u NONE -c "set rtp^=$(pwd)" -c "luafile tests/lua/spec.lua"
--- scripts/test-vim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-cd "$(dirname "$0")/.."
-exec vim -Nu NONE -n -es -S tests/vim/spec.vim
